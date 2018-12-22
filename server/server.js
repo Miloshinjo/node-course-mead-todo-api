36e510858f8a98ebf813e4c310412f0ed10c46b9
milos.dzeletovic <milos.dzeletovic@consulteer.com>
2018-12-22 17:52:31 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express()
 // configure middleware to use body-parser
 app.use(bodyParser.json())
 
-// create a post route for todos
+// create a POST route for todos
 app.post('/todos', (req, res) => {
   // create a new todo
   const todo = new Todo({
@@ -19,6 +19,16 @@ app.post('/todos', (req, res) => {
   // save todo to database
   todo.save().then((doc) => {
     res.send(doc)
+  }, (err) => {
+    res.status(400).send(err)
+  })
+})
+
+// create a GET route for todos
+app.get('/todos', (req, res) => {
+  // get all todos from the database
+  Todo.find().then((todos) => {
+    res.send({ todos })
   }, (err) => {
     res.status(400).send(err)
   })
